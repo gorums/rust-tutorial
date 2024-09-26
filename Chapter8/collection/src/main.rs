4f592bc08f56da0c688b70b138f307b1a559694c
Alejandro <acksecurity@hotmail.com>
2024-09-26 11:58:20 -0400

    feat: Add string manipulation examples in Chapter 8 collection module

--- Chapter8/collection/src/main.rs
+++ Chapter8/collection/src/main.rs
@@ -24,6 +24,18 @@ fn main() {
         *i += 50;
     }
 
-    
+    let mut s = String::new();
+    let data = "initial contents";
+    let s = data.to_string();
+    let s = "initial contents".to_string();
+    let s = String::from("initial contents");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = s1 + "-" + &s2 + "-" + &s3;
+
+
     println!("Hello, world!");
 }
